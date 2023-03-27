746fb5a520843cba6a5639bb0592a19e1fdd0ef5
UlianaGuseva <ulianaandrevna@gmail.com>
2023-03-28 00:57:43 +0300

    day2

--- Day1/loops.py
+++ Day1/loops.py
@@ -14,3 +14,35 @@ for char in txt:
     print(char)
     
 # While
+
+# if True:
+    # print('hello')
+
+while True:
+    print('hello')
+    
+    
+    username = input('usrnm')
+    password = input('pswrd')
+    
+    if username == "yossi":
+        break
+    elif password == 'Tomato':
+        continue
+    else:
+        print('smth')
+        
+    tries <3
+    username = input('usrnm')
+    password = input('pswrd')
+    
+    if username == "yossi":
+        password = input('pswrd')
+            if password == 'Tomato':
+                print('welcome')
+                break
+            else:
+                tries +=1
+                print(f'Tries'+ tries)
+    
+    
