a3646cb1bdb4608a016e7409c669838645bcaa0c
UlianaGuseva <ulianaandrevna@gmail.com>
2023-04-16 15:04:34 +0300

    w4d1_daily

---
+++ Week4/daily.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE actors(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+-- )
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Angelina ','Jolie ','04/06/1975', 1);
+
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Scarlett','Johansson ','22/11/1984', 2);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES
+-- ('Johnny','Depp ','09/06/1963', 3),
+-- ('George','Clooney ','05/06/1961', 2),
+-- ('Brad','Pitt ','18/12/1963', 2);
+
+-- select count(first_name) from actors
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('',' ','22/11/1984', 2);
