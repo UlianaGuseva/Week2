3ab1ac3a9b5b2f7b4dd22482b05cab80025e5ae9
UlianaGuseva <ulianaandrevna@gmail.com>
2023-04-22 19:24:55 +0300

    hw-w5d1daily

--- Week5/Day1HW/daily_people/polls/views.py
+++ Week5/Day1HW/daily_people/polls/views.py
@@ -33,5 +33,19 @@ def people_info(request, id):
     return render(request, 'index.html', context)
 
 def people_all(request): 
-    context = people
-    return render(request, 'index.html', context)
+    to_sort_people = people
+    sorted_people = []
+    ages = []
+    for i in to_sort_people:
+        ages.append(i['age'])
+    ages.sort() 
+    for n in ages:
+        for c in to_sort_people:
+            if c['age'] == n:
+                # to_sort_people.remove(c)
+                sorted_people.append(c)
+    context = {'people': sorted_people}
+    # for i in range(1, len(people)+1):
+    #     context[i] = people[i-1]['name'] + people[i-1]['age'] + people[i-1]['country']
+    #     return context
+    return render(request, 'all_people.html', context)
